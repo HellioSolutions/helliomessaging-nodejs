6d6d5859876123229c1a9ad5c2db734a432beb85
Albert Ninyeh <eaglesecurity0@gmail.com>
2023-06-20 13:56:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,162 @@
+# Hellio Messaging Node.js Package
+
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](LICENSE)
+
+A Node.js package for integrating with Hellio Messaging API. This package provides functionalities for sending SMS, generating OTP, performing number lookup, checking delivery reports, checking sms and voice pricing and checking account balance.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Installation
+
+Describe the installation steps required to set up and run your project. Include any dependencies and prerequisites.
+
+```bash
+npm install hellio-messaging
+
+```
+
+## Usage
+
+```js
+
+const HellioMessaging = require("hellio-messaging");
+
+// Initialize the Hellio Messaging client
+const client = new HellioMessaging({
+    clientId: "yourClientId",
+    applicationSecret: "yourApplicationSecret",
+});
+
+// Send an SMS
+const smsParams = {
+    senderId: "yourSenderId",
+    msisdn: "recipientPhoneNumber",
+    message: "Hello, World!",
+};
+
+client
+    .sendSMS(smsParams)
+    .then((response) => {
+        console.log("SMS sent successfully:", response);
+    })
+    .catch((error) => {
+        console.error("Failed to send SMS:", error);
+    });
+
+// Generate OTP
+const otpParams = {
+    mobile_number: "recipientPhoneNumber",
+    timeout: 60,
+    message: "Your verification code is: {OTP}",
+    senderId: "yourSenderId",
+    tokenlength: 6,
+    recipient_email: "recipientEmail",
+    messageType: 0,
+};
+
+client
+    .sendOTP(otpParams)
+    .then((response) => {
+        console.log("OTP sent successfully:", response);
+    })
+    .catch((error) => {
+        console.error("Failed to send OTP:", error);
+    });
+
+// Verify OTP
+const verifyParams = {
+    mobile_number: "recipientPhoneNumber",
+    otp: "otpCode",
+};
+
+client
+    .verifyOTP(verifyParams)
+    .then((response) => {
+        console.log("OTP verification successful:", response);
+    })
+    .catch((error) => {
+        console.error("OTP verification failed:", error);
+    });
+
+// Perform number lookup
+const lookupParams = {
+    msisdn: "phoneNumber",
+    reference: "campaignName",
+};
+
+client
+    .numberLookup(lookupParams)
+    .then((response) => {
+        console.log("Number lookup result:", response);
+    })
+    .catch((error) => {
+        console.error("Number lookup failed:", error);
+    });
+
+// Get routing price
+const routingParams = {
+    country_code: "GH",
+    service_name: "sms", //sms or voice
+};
+
+client
+    .getRoutingPrice(routingParams)
+    .then((response) => {
+        console.log("Routing price:", response);
+    })
+    .catch((error) => {
+        console.error("Failed to retrieve routing price:", error);
+    });
+
+// Check delivery report
+const reportParams = {
+    startDate: "startDate",
+    endDate: "endDate",
+    senderId: "yourSenderId",
+    deliveryStatus: "DELIVRD",
+};
+
+client
+    .checkDeliveryReport(reportParams)
+    .then((response) => {
+        console.log("Delivery report:", response);
+    })
+    .catch((error) => {
+        console.error("Failed to check delivery report:", error);
+    });
+
+// Check account balance
+client
+    .checkBalance()
+    .then((balance) => {
+        console.log("Account balance:", balance);
+    })
+    .catch((error) => {
+        console.error("Failed to check account balance:", error);
+    });
+
+
+
+```
+
+
+## Features
+
+For more details on the available methods and parameters, please refer to the Hellio Messaging Node.js package documentation.
+
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT](LICENSE)
+
+Feel free to customize the README.md file further to add additional details specific to your project, such as examples, usage guidelines, and any other relevant information.
---
+++ src/index.js
@@ -0,0 +1,137 @@
+const axios = require("axios");
+const moment = require("moment");
+const sha1 = require("sha1");
+
+class HellioMessaging {
+    constructor(options) {
+        this.clientId = options.clientId;
+        this.applicationSecret = options.applicationSecret;
+        this.version = options.version || 2;
+        this.baseURL = options.baseURL || "https://api.helliomessaging.com";
+        this.api = axios.create({
+            baseURL: this.baseURL,
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${this.getAuthToken()}`,
+            },
+        });
+    }
+
+    //Authenticate yourself on the Hellio Messaging platform
+    getAuthToken() {
+        const utcMoment = moment.utc();
+        const currentDateTime = utcMoment.format("YYYYMMDDHH");
+        const hashedAuthKey = sha1(this.clientId + this.applicationSecret + currentDateTime);
+        return hashedAuthKey;
+    }
+
+    //Send SMS
+    sendSMS(params) {
+        const endpoint = "/sms";
+
+        const payload = {
+            senderId: params.senderId,
+            msisdn: params.msisdn,
+            message: params.message,
+        };
+
+        return this.api
+            .post(endpoint, payload)
+            .then((response) => response.data)
+            .catch((error) => Promise.reject(error.response.data));
+    }
+
+    //Send Otp
+    sendOTP(params) {
+        const endpoint = "/v3/otp/send";
+
+        const payload = {
+            mobile_number: params.mobile_number,
+            timeout: params.timeout,
+            message: params.message,
+            senderId: params.senderId,
+            tokenlength: params.tokenlength,
+            recipient_email: params.recipient_email,
+            messageType: params.messageType,
+        };
+
+        return this.api
+            .post(endpoint, payload)
+            .then((response) => response.data)
+            .catch((error) => Promise.reject(error.response.data));
+    }
+
+    //Verify otp codes
+    verifyOTP(params) {
+        const endpoint = "/v3/otp/verify";
+
+        const payload = {
+            mobile_number: params.mobile_number,
+            otp: params.otp,
+        };
+
+        return this.api
+            .post(endpoint, payload)
+            .then((response) => response.data)
+            .catch((error) => Promise.reject(error.response.data));
+    }
+
+    //Perform a number lookup request
+    numberLookup(params) {
+        const endpoint = "/hlr/request";
+
+        const payload = {
+            msisdn: params.msisdn,
+            reference: params.reference,
+        };
+
+        return this.api
+            .post(endpoint, payload)
+            .then((response) => response.data)
+            .catch((error) => Promise.reject(error.response.data));
+    }
+
+    // Get the routing price for SMS and Voice
+    getRoutingPrice(params) {
+        const endpoint = "/route/pricing";
+
+        const queryParams = {
+            country_code: params.country_code,
+            service_name: params.service_name,
+        };
+
+        return this.api
+            .get(endpoint, { params: queryParams })
+            .then((response) => response.data)
+            .catch((error) => Promise.reject(error.response.data));
+    }
+
+    //Get delivery report logs
+    checkDeliveryReport(params) {
+        const endpoint = "/delivery-logs";
+
+        const queryParams = {
+            startDate: params.startDate,
+            endDate: params.endDate,
+            senderId: params.senderId,
+            deliveryStatus: params.deliveryStatus,
+        };
+
+        return this.api
+            .get(endpoint, { params: queryParams })
+            .then((response) => response.data)
+            .catch((error) => Promise.reject(error.response.data));
+    }
+
+    //Check your Hellio Messaging account balance
+    checkBalance() {
+        const endpoint = "/credit-balance";
+
+        return this.api
+            .get(endpoint)
+            .then((response) => response.data.balance)
+            .catch((error) => Promise.reject(error.response.data));
+    }
+}
+
+module.exports = HellioMessaging;
